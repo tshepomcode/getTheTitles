d51ca5770754daba4c0768cf054d02a7a275e254
tshepomcode <tshepomcode@gmail.com>
2020-05-17 11:29:26 +0200

    Completed

--- getTheTitles.js
+++ getTheTitles.js
@@ -1,5 +1,24 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // debugger;
+    var titles = books.map(function(value){
+        return value.title;
+    });
 
+    return titles;
 }
 
 module.exports = getTheTitles;
+
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+
+
+// console.log(getTheTitles(books));
